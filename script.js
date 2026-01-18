71a51c14d2b9a6cc5071f84da765f4d5b3281fef
brebierod <bierodbre@gmail.com>
2026-01-17 21:23:18 -0500

    Duplicate 16 styled rows

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+body = document.querySelector("body");
+let rows = [];
 //Create 1 row
+function makeRow() {
+    const row = document.createElement("div");
+   
+    return row;
+}
+
 //Specify row height and width
+
 //Iterate 16 rows
+
+function makeRows(type) {
+    for (let i = 0; i < 16; i++) {
+        const duplicate = document.createElement("div");
+        duplicate.setAttribute("style", "outline: 1px, lightblue, solid; width: 800px; height: 50px; background-color: beige");
+        rows.push(duplicate)
+        body.appendChild(rows[i]);
+    }
+};
 //Create 1 square
 //Iterate 16 squares per row
 //Append to row 1 
 //Repeat until all rows are filled
+//Append to body
